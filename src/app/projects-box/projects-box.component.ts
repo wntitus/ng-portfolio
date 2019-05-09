b1844d753927dd0c392e00c8dcfd2f8ce0a50432
Wesley Titus <wesn.titus@gmail.com>
2019-05-08 21:36:02 -0400

    adding project data

--- src/app/projects-box/projects-box.component.ts
+++ src/app/projects-box/projects-box.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects-box.component.css']
 })
 export class ProjectsBoxComponent implements OnInit {
-  const projects = [
+  projects = [
     {
       name: 'StudyHive',
-      repoLink: 'https://github.com/wntitus/studyhive'
+      repoLink: 'https://github.com/wntitus/studyhive',
+      description: `This project is a personal favorite of mine. It's the project where I started
+                    using Angular and ended up really liking it. It's a full-stack MEAN application
+                    to track study resources and notes. I implemented authorization using bcrypt and
+                    JSON web tokens. It communicates with a REST Node backend to create and check users
+                    from a cloud-hosted Mongo database.`
+    }, {
+      name: 'DevDen',
+      repoLink: 'https://github.com/wntitus/DevDen',
+      description: `This is the project that made me really love working cooperatively on projects.
+                    It's a full-stack app utilizing a MySQL database with Passport authentication
+                    that serves as a hub for developers to create projects and find collaborators for
+                    their projects. We also managed to implement a live chat using websockets, which
+                    was a lot of fun.`
     }
   ];
 
